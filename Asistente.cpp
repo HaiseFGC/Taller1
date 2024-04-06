ae07005dac006fecee122459807cf79b395914a1
HaiseFGC <112604718+HaiseFGC@users.noreply.github.com>
2024-04-05 21:34:05 -0300

    Create Asistente.cpp

---
+++ Asistente.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Asistente.h"
+using namespace std;
+
+Asistente::Asistente(string nombre, int edad, string ocupacion, string lugar){
+    this -> nombre = nombre;
+    this -> edad = edad;
+    this -> ocupacion = ocupacion;
+    this -> lugar = lugar;
+}
+
+Asistente::Asistente(){
+
+}
+
+string Asistente::getNombre() const{return nombre;}
+int Asistente::getEdad() const{return edad;}
+string Asistente::getOcupacion() const{return ocupacion;}
+string Asistente::getLugar() const{ return lugar;}
+
+void Asistente::printDetalles() const {
+    cout << "Nombre: " << nombre << endl;
+    cout << "Edad: " << edad << endl;
+    cout << "Ocupación: " << ocupacion << endl;
+    cout << "Lugar: " << lugar << endl;
+}
